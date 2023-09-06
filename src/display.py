b19a49ba478ee59d15d999ba78a2e56e5e0198b5
bitSheriff <bitSheriff@protonmail.com>
2023-09-06 15:14:36 +0100

    remove own driver

--- src/display.py
+++ src/display.py
@@ -3,10 +3,8 @@ import os
 import sys
 from PIL import Image
 import epaper
-import src.epdDriver as epdDriver
 import logging
 
-USE_OWN_DRIVER = 0
 
 class display:
 
@@ -21,10 +19,7 @@ class display:
 
     def __initEPD(self, type):
 
-        if USE_OWN_DRIVER:
-            self.__epd = epdDriver.EPD()
-        else:
-            self.__epd = epaper.epaper('epd7in5_V2').EPD()
+        self.__epd = epaper.epaper('epd7in5_V2').EPD()
 
         self.__epd.init()
 
@@ -42,7 +37,7 @@ class display:
         image = image.rotate(90)
         new_size = (800, 480)
 
-        #image.thumbnail(new_size)  # Use Image.ANTIALIAS for high-quality resizing
+        image.thumbnail(new_size)  # Use Image.ANTIALIAS for high-quality resizing
 
         image = image.resize(new_size)
 
@@ -57,19 +52,14 @@ class display:
         logging.debug(f"given image: {path}")
         self.__image = Image.open(path)
 
-        if not USE_OWN_DRIVER:
-            # convert the image to the correct colors
-            if self.__colors == "BW":
-               self.__image = self.__image.convert(mode="L", dither=Image.FLOYDSTEINBERG)    # TODO "L" or "1", needs to be checked on hardware
-
+        # convert the image to the correct colors
+        if self.__colors == "BW":
+            self.__image = self.__image.convert(mode="L", dither=Image.FLOYDSTEINBERG)    # TODO "L" or "1", needs to be checked on hardware
 
         self.__image = self.__resize_image(self.__image)
 
-        if USE_OWN_DRIVER:
-            self.__epd.display_frame(self.__epd.get_frame_buffer(self.__image))
-        else:
-            self.__epd.display(self.__epd.getbuffer(self.__image))
-            self.__epd.sleep()
+        self.__epd.display(self.__epd.getbuffer(self.__image))
+        self.__epd.sleep()
 
         pass
 
