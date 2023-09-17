1f647415aee860843e20c618cd876ab990855aba
bitSheriff <bitSheriff@protonmail.com>
2023-09-17 17:01:03 +0200

    added script to a service

--- autostart_shelfslide.sh
+++ autostart_shelfslide.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
-# copy this file to "~/.config/autostart/"
 
-python3 ~/ShelfSlide/shelfslide.py
+# Path to the Python program
+PROGRAM_PATH="$HOME/ShelfSlide/shelfslide.py"
+
+# Name of the systemd service
+SERVICE_NAME="shelfslide"
+
+# Get the username of the person running the script
+CURRENT_USER="$USER"
+
+# Create the unit file
+cat <<EOF > /etc/systemd/system/${SERVICE_NAME}.service
+[Unit]
+Description=ShelfSlide Python Program
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/python3 ${PROGRAM_PATH}
+WorkingDirectory=$HOME/ShelfSlide
+StandardOutput=inherit
+StandardError=inherit
+Restart=always
+User=$CURRENT_USER
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd
+systemctl daemon-reload
+
+# Enable and start the service
+systemctl enable ${SERVICE_NAME}
+systemctl start ${SERVICE_NAME}
+
+# Display the status of the service
+systemctl status ${SERVICE_NAME}
